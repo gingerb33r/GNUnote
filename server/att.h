760c4305b98e88ffebcef0b51f76ff535bc9cf18
gingerb33r <gingerbeer@riseup.net>
2013-12-11 17:15:12 +1100

    Added more structs, will expand soon.

--- server/att.h
+++ server/att.h
@@ -1,13 +1,15 @@
 typedef struct user_info{
-    const char *username;
-    const char *email;
-    const char *password_unhashed;
+    const char *username; /*For example: gingerb33r*/
+    const char *email; /*For example: interjection@gnu.com*/
+    const char *password_unhashed; /*An unhashed/plaintext password; for example: "password".
+                                     Hashed passwords are stored in the database, the DB will
+                                     not store any plaintext password.*/
 } USER;
 
 typedef struct thought{ /*Structure of a note, information will be added to the DB.*/ 
     int note_num; /*Categorizes the note in order. This will be sent back to the user.*/
-    const char *title; /**/
-    const char *text;
+    const char *title; /*Title of the note; for example: "Tomorrow's work"*/
+    const char *text; /*Contents of the note; for example: "Do something."*/
 } NOTE;
 
 typedef struct MySQL_Details{ /*Wiil be used in conjunction with MySQL linker. This is just for reference.*/
