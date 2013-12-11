9788ea62667d4ed9a137a567cdef7cd697cbc12c
gingerb33r <gingerbeer@riseup.net>
2013-12-11 17:07:19 +1100

    Added more structs, will expand soon.

--- server/att.h
+++ server/att.h
@@ -1,4 +1,16 @@
-typedef struct thought{
-    const char *title;
+typedef struct user_info{
+    const char *username;
+    const char *email;
+    const char *password_unhashed;
+} USER;
+
+typedef struct thought{ /*Structure of a note, information will be added to the DB.*/ 
+    int note_num; /*Categorizes the note in order. This will be sent back to the user.*/
+    const char *title; /**/
     const char *text;
 } NOTE;
+
+typedef struct MySQL_Details{ /*Wiil be used in conjunction with MySQL linker. This is just for reference.*/
+    const char *IP; /*For example: 127.0.0.1*/
+    const char *DB; /*Database name, for example: wreckinskrubs*/
+} MYSQL_INFO;
