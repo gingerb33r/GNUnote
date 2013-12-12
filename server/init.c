b7011367bb4d2698bd3fc0b7eb988eb9180b5dcc
gingerb33r <gingerbeer@riseup.net>
2013-12-12 12:42:24 +1100

    Testing a commit.

--- server/init.c
+++ server/init.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#include "att.h"
+
+void save_one()
+{
+    printf("Hello!\n");
+}
+
+void add_note(int note_num, const char *title, const char *text, NOTE *note, 
+        void (*saveM)())
+{
+    (*saveM)();
+    note->title = title;
+    printf("%s\n", note->title);
+}
 
 int main(int argc, char **argv)
 {
